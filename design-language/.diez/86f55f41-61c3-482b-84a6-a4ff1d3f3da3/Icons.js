2252c48a56595143a0c4b4aa600d2cdf1a8e6681
Taylor Poe <taylorpoe@gmail.com>
2019-12-04 10:48:38 -0800

    chore: ignore .diez & node_modules folders

--- design-language/.diez/86f55f41-61c3-482b-84a6-a4ff1d3f3da3/Icons.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const prefabs_1 = require("@diez/prefabs");
-exports.icons = {
-    logo: prefabs_1.Image.responsive('assets/logo.png', 52, 48),
-    masthead: prefabs_1.Image.responsive('assets/masthead.png', 208, 88),
-};
--- design-language/node_modules/@diez/extractors-core/types/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './api';
-export * from './utils';
-//# sourceMappingURL=index.d.ts.map

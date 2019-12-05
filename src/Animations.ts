286b0bc2d04d31adbb2778e4b4ede6282fb73a3c
Taylor Poe <taylorpoe@gmail.com>
2019-12-05 10:18:49 -0800

    fix: file naming cont'

--- src/Animations.ts
+++ src/Animations.ts
@@ -1,5 +1,5 @@
 import { Lottie } from '@diez/prefabs';
 
 export const animations = {
-  404: Lottie.fromJson('assets/404.json', true),
+  fourOfour: Lottie.fromJson('assets/fourOfour.json', true),
 }

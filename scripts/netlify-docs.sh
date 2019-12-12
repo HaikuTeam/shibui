4faca84767d59e40deeea456c5e70806b12236f3
Roberto Dip <dip.jesusr@gmail.com>
2019-12-12 17:56:31 -0300

    tweak: adjust deploy script

--- scripts/netlify-docs.sh
+++ scripts/netlify-docs.sh
@@ -1,14 +1,14 @@
-# yarn install
-# yarn diez compile -t docs
+yarn install
+yarn diez compile -t docs
 
-# pushd build/docs
-#   yarn install
-# popd
+cd build/docs
+yarn install
+cd ../..
 
-# mkdir netlify
+mkdir netlify
 
-pushd netlify
-  cp -r ../build/docs/node_modules/@diez/docs-template-app/dist/* .
-  cp ../build/docs/tree.json assets
-  cp ../build/docs/searchIndex.json assets
-popd
+cd netlify
+cp -r ../build/docs/node_modules/@diez/docs-template-app/dist/* .
+cp ../build/docs/tree.json assets
+cp ../build/docs/searchIndex.json assets
+cd ..

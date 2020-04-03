a06e7b0c0cea6d71fe11cfdb298271a23734411d
Roberto Dip <rob@haiku.ai>
2020-04-03 12:30:42 -0700

    feat: use open source version of docs and simplify deploy scripts (#2)

--- scripts/netlify-docs.sh
+++ scripts/netlify-docs.sh
@@ -1,14 +1,7 @@
 yarn install
 yarn diez compile -t docs
 
-cd build/docs
-yarn install
-cd ../..
-
-mkdir netlify
-
-cd netlify
-cp -r ../build/docs/node_modules/@diez/docs-template-app/dist/* .
-cp ../build/docs/tree.json assets
-cp ../build/docs/searchIndex.json assets
-cd ..
+pushd build/diez-haiku-shibui-docs
+yarn build
+cp -r dist ../../dist
+popd
